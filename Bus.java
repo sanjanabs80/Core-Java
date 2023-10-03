a4e992004b57b174b2a2d077bcf1d5be9588685d
bssanjana80 <bssanjana80@gmail.com>
2023-10-03 22:51:46 +0530

    this keyword

---
+++ Bus.java
@@ -0,0 +1,41 @@
+public class Bus{
+	int busNo;
+	String color;
+	int numberofSeats;
+	String busType;
+	
+	Bus(int busNo,String color,int numberofSeats,String busType){
+		this(busNo,color,numberofSeats);
+		this.busType=busType;
+	}
+	Bus(int busNo,String color,int numberofSeats){
+		this(busNo,color);
+		this.numberofSeats=numberofSeats;
+	}
+	Bus(int busNo,String color){
+		this(busNo);
+		this.color=color;
+	}
+	Bus(int busNo){
+		this.busNo=busNo;
+	}
+	public static void main(String[] args)
+	{
+		Bus bus=new Bus(500,"red",45,"bmtc");
+		Bus bus1=new Bus(25,"blue",30,"volvo");
+		
+		System.out.println(bus.color);
+		System.out.println(bus1.busType);
+		
+		
+		Bus bus2=new Bus(25,"blue",30,"volvo");
+		System.out.println(bus.numberofSeats);
+				System.out.println(bus1.color);
+		
+
+	
+	}
+	
+	
+}
+
---
+++ Car.java
@@ -0,0 +1,32 @@
+public class Car{
+String modelName;
+int speed;
+String color;
+
+Car(String modelName,int speed,String color){
+	this(modelName,speed);
+	this.color=color;
+}
+Car(String modelName,int speed){
+	this(modelName);
+	this.speed=speed;
+	
+}
+Car(String modelName){
+	this.modelName=modelName;
+}
+public static void main(String[] args){
+	Car obj1=new Car("benz",180,"red");
+	Car obj2=new Car("BMW",100,"white");
+	
+	System.out.println(obj1.modelName);
+	System.out.println(obj2.modelName);
+	
+	System.out.println(obj1.speed);
+	System.out.println(obj2.speed);
+		
+	System.out.println(obj1.color);
+	System.out.println(obj2.color);
+	
+}
+}
