1dfa8e641f5bea1eb289150ad98dc07d3885790c
bssanjana80 <bssanjana80@gmail.com>
2023-10-05 11:10:32 +0530

    strings

---
+++ Things.java
@@ -0,0 +1,63 @@
+public class Things{
+
+	public static void main(String[] args)
+	{	
+		
+		String car="benz";
+		String bike="honda";
+		String pen="ball point";
+		String tv="watch movies";
+		String water="drinking";
+		String bag="carry";
+		String laptop="work";
+		String mobile="get info";
+		String bank="savings";
+		String bus="travel";
+		
+		
+	    car="BMW";
+		pen="ink";
+		tv="play games";
+		bag="store";
+		bank="withdraw";
+		
+		
+		String obj= new String("welcome to java");
+		String concepts= new String("strings");
+		String definition= new String("sequence of characters");
+		String enclosed= new String("double quotes");
+		String obj1= new String("constructors");
+		String types= new String("3 types");
+		String rule1= new String("constructor is same  as class name");
+		String rule2= new String("no return type");
+		String firstType= new String("default constructor");
+		String secondType= new String("parameter constructor");
+		
+		
+		obj="welcome tojava programing";
+		concepts="constructor chaining";
+		types="no Parameter Constructor";
+		obj1="class1";
+		definition="class is Collection of Objects";
+	
+		
+		System.out.println(car==car);
+		System.out.println(pen==bank);
+		System.out.println(tv==tv);
+		System.out.println(bag==bus);
+		System.out.println(bank==bank);
+		
+		System.out.println(obj==firstType);
+		System.out.println(concepts==concepts);
+		System.out.println(types==rule1);
+		System.out.println(obj1==obj1);
+		System.out.println(definition==concepts);
+		
+		System.out.println(car==obj);
+		System.out.println(definition==definition);
+		System.out.println(bag==bank);
+		System.out.println(water==water);
+		System.out.println(bus==rule1);
+	}
+}
+	
