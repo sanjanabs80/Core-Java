0da86d80bbd04149f06e6687cada6d1e9d6bf453
bssanjana80 <bssanjana80@gmail.com>
2023-10-09 13:11:41 +0530

    static variables

---
+++ Door.java
@@ -0,0 +1,41 @@
+public class Door {
+     String color;
+     int height;
+     int width;
+     boolean isOpen;
+	 boolean hasHandle;
+     int numberOfDoors;
+	 int numberOfLockers;
+	 static String material;
+	  static String defaultColor;
+	  static boolean isLocked;
+	 
+	 static{
+	 material="wooden";
+      defaultColor = "White";
+     isLocked = false;
+	 }
+    Door(String color,int height,int width,boolean isOpen,boolean hasHandle,int numberOfDoors,int numberOfLockers)
+	{
+		this.color=color;
+		this.height=height;
+		this.width=width;
+		this.isOpen=isOpen;
+		this.hasHandle=hasHandle;
+		this.numberOfDoors=numberOfDoors;
+		this.numberOfLockers=numberOfLockers;
+		
+	}
+	public static void main(String[] args)
+	{
+		Door door=new Door("brown",6,7,true,true,2,3);
+		System.out.println(door.color);
+		
+		Door door1=new Door("white",4,8,false,true,3,4);
+		System.out.println(door1.isOpen);
+		
+		Door door2=new Door("black",5,2,true,false,6,6);
+		System.out.println(door2.numberOfDoors);
+		
+	}
+}
