7195f5d4593a0c3adc433c1ed773a9b92a25cce6
bssanjana80 <bssanjana80@gmail.com>
2023-11-24 11:42:12 +0530

    override

--- Application_override/Chrome.java
+++ Application_override/Chrome.java
@@ -5,43 +5,51 @@ public class Chrome extends Browser{
 	double version,String developer,String date){
 		super(downloads,browsingHistorySites,size,compatiblewith,os,id,name,type,version,developer,date);
 	}
+	@override
 	
 	 void run(){
 	 System.out.println("application running in chrome");
  }
+ @override
  void stop(){
 	 System.out.println("stop running application in chrome");
 	
  }
+ @override
  void execute(){
 	 System.out.println("application execute in chrome");
 	 
  }
- 
+ @override 
  void consumeMemory(){
 		System.out.println("application consumeMemory in chrome");
 	
 	}
+	@override
 	void connectInternet(){
 		System.out.println("application consumeInternet in chrome");
 	}
+	@override
  void collectInfo(){
 	 System.out.println("application collects the information");
  }
+ @override
  void displayInfo(){
 	 System.out.println("display info");
 	 
  } 
+ @override
 	  
 	 void sendUsage(){
 		 System.out.println("send usage in chrome");
 	 }
 
+	 @override
 void settings(){
 		System.out.println("application settings");
 		
 	}
-	
+	@override
 	void displayHistory(){
 		System.out.println("display history in chrome");
 	}
--- Application_override/FireFox.java
+++ Application_override/FireFox.java
@@ -5,39 +5,49 @@ public class FireFox extends Browser{
 	double version,String developer,String date){
 		super(downloads,browsingHistorySites,size,compatiblewith,os,id,name,type,version,developer,date);
 	}
+	@override
  void run(){
 	 System.out.println("application running ");
  }
+ @override
  void stop(){
 	 System.out.println("stop running application ");
 	
  }
+ @override
  void execute(){
 	 System.out.println("application execute ");
 	 
  }
+ @override
  
  void consumeMemory(){
 		System.out.println("application consumeMemory ");
 	
 	}
+	@override
 	void connectInternet(){
 		System.out.println("application consumeInternet ");
 	}
+	@override
  void collectInfo(){
 	 System.out.println("application collects the information");
  }
+ @override
  void displayInfo(){
 	 System.out.println("display info");
 	
 	 }
+	 @override
 	 void sendUsage(){
 		 System.out.println("send usage ");
 	 }
+	 @override
 void settings(){
 		System.out.println("application settings");
 		
 	}
+	@override
 	
 	void displayHistory(){
 		System.out.println("display history ");
