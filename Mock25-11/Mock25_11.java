4d8b123a210cd4df94bac961c5b7c674716e9e85
bssanjana80 <bssanjana80@gmail.com>
2023-11-25 12:32:47 +0530

    mock

---
+++ Mock25-11/Mock25_11.java
@@ -0,0 +1,34 @@
+1)public class Number{
+    public static void main(String[] args){
+         int num=5;
+       boolean flag=false;
+       for(int i=2;i<num;i++){
+         if(num %i==0){
+           flag=true;
+               true;
+}
+            if(flag){
+        System.out.println(num+"prime number");
+
+          else{
+
+        System.out.println(num+"not a prime number");
+}
+}
+
+
+2) public class Str{
+     String name="java";
+
+
+
+
+3) public class Demo{
+     String str="XworkZ Odc";
+
+public static void main(String[] args){
+System.out.println(str.toUpper());
+}
+}
+
+
